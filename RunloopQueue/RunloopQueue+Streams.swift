c5984014b2aceb45c7dc3d76de39dff7fccd3b3d
Daniel Kennett <daniel@cascable.se>
2017-07-15 15:32:12 +0200

    Add convenience methods for scheduling NSURLConnections

--- RunloopQueue/RunloopQueue+Streams.swift
+++ RunloopQueue/RunloopQueue+Streams.swift
@@ -13,6 +13,7 @@ public extension RunloopQueue {
     /// Schedules the given stream into the queue.
     ///
     /// - Parameter stream: The stream to schedule.
+    @objc(scheduleStream:)
     public func schedule(_ stream: Stream) {
         if let input = stream as? InputStream {
             sync { CFReadStreamScheduleWithRunLoop(input as CFReadStream, CFRunLoopGetCurrent(), .commonModes) }
@@ -24,6 +25,7 @@ public extension RunloopQueue {
     /// Removes the given stream from the queue.
     ///
     /// - Parameter stream: The stream to remove.
+    @objc(unscheduleStream:)
     public func unschedule(_ stream: Stream) {
         if let input = stream as? InputStream {
             sync { CFReadStreamUnscheduleFromRunLoop(input as CFReadStream, CFRunLoopGetCurrent(), .commonModes) }
---
+++ RunloopQueue/RunloopQueue+URLConnection.swift
@@ -0,0 +1,33 @@
+//
+//  RunloopQueue+Streams.swift
+//  RunloopQueue
+//
+//  Created by Daniel Kennett on 2017-02-14.
+//  Copyright © 2017 Cascable AB. All rights reserved.
+//
+
+import Foundation
+
+public extension RunloopQueue {
+
+    /// Schedules the given NSURLConnection into the queue.
+    ///
+    /// - Parameter stream: The connection to schedule.
+    @objc(scheduleConnection:)
+    public func schedule(_ connection: NSURLConnection) {
+        sync {
+            connection.schedule(in: RunLoop.current, forMode: RunLoopMode.commonModes)
+        }
+    }
+
+    /// Removes the given NSURLConnection from the queue.
+    ///
+    /// - Parameter stream: The connection to remove.
+    @objc(unscheduleConnection:)
+    public func unschedule(_ connection: NSURLConnection) {
+        sync {
+            connection.unschedule(from: RunLoop.current, forMode: RunLoopMode.commonModes)
+        }
+    }
+
+}
